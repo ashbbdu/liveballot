7789dd505c70a9cf0d7896667cf2da9ef1e6f693
ashbbdu <ashishsrivastava7007@gmail.com>
2025-07-24 20:47:42 +0530

    created first poll

--- src/poll/poll.repository.ts
+++ src/poll/poll.repository.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 
 import { InjectModel } from '@nestjs/sequelize';
 import { Poll } from './poll.entitiy';
-import { Request } from 'express';
 
 @Injectable()
 export class PollRepository {
@@ -11,11 +10,10 @@ export class PollRepository {
     private readonly pollModel: typeof Poll,
   ) {}
 
-  async createPoll (data : any , req : any) {
-    console.log(data , "dataaaa")
-    const payload = {...data , createdBy : req.user.userId}
+  async createPoll(data: any, req: any) {
+    console.log(data, 'dataaaa');
+    const payload = { ...data, createdBy: req.user.userId };
     const poll = await this.pollModel.create(payload);
     return poll;
   }
-
 }
